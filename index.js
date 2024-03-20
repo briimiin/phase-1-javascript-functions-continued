3a3520bcee1b784eff1dbd970e2ec8a5d94add59
briimiin <kipkorirbrimin005@gmail.com>
2024-03-20 20:51:45 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+console.log(saturdayFun("go swimming"));
+function mondayWork(todo = "go to the office"){
+    return`This Monday, I will ${todo}.`;
+}
+console.log(saturdayFun());
+console.log(saturdayFun("go to my moms"));
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer")); 
